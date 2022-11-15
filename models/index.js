5e2fdf72f32bb202afd1aa89ffb8764b8fa98c4f
Jackie <jackierhammer@gmail.com>
2022-11-14 20:00:05 -0700

    established foreign key relationships

--- models/index.js
+++ models/index.js
@@ -1,1 +1,31 @@
 // this is where foreign key relationships will go
+
+// imports all three model types 
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
+
+// posts belong to users
+Post.belongsTo(User, {
+    foreignKey: 'userId',
+    onDelete: 'CASCADE'
+});
+
+// comments belong to users
+Comment.belongsTo(User, {
+    foreignKey: 'userId',
+    onDelete: 'CASCADE'
+});
+
+// posts have comments
+Post.hasMany(Comment, {
+    foreignKey: 'postId',
+    onDelete: 'CASCADE'
+});
+
+// exports updated model relationships
+module.exports = {
+    User,
+    Post,
+    Comment
+};
