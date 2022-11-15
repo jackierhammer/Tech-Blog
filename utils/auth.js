8f48677a13f98c3190d1d060fc9cb164dc649040
Jackie <jackierhammer@gmail.com>
2022-11-14 19:36:30 -0700

    updated utils helper functions

--- utils/auth.js
+++ utils/auth.js
@@ -1,1 +1,12 @@
 // this is where withAuth helper function will go 
+
+const withAuth = (req, res, next) => {
+    // If the user is not logged in, redirect the request to the login route
+    if (!req.session.logged_in) {
+      res.redirect('/login');
+    } else {
+      next();
+    }
+  };
+  
+  module.exports = withAuth;
--- utils/helpers.js
+++ utils/helpers.js
@@ -1,1 +1,8 @@
 // this is where time stamp helper function will go
+
+module.exports = {
+    format_date: date => {
+        // Format date as MM/DD/YYYY
+        return date.toLocaleDateString();
+    },
+};
