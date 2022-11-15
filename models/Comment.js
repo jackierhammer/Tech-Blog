0e8c6df6c02518017e69d89b4800467a38ec154d
Jackie <jackierhammer@gmail.com>
2022-11-14 19:51:45 -0700

    worked on comment model

--- models/Comment.js
+++ models/Comment.js
@@ -1,2 +1,23 @@
 // this is where the comment model will go
 
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+// creates Comment model
+class Comment extends Model {}
+
+// comment only needs a body; the important relationships will be established with foreign keys
+Comment.init(
+  {
+    body: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  },
+  {
+    sequelize
+  }
+);
+
+// exports Comment model
+module.exports = Comment;
