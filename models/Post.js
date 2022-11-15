89dd0435c6a555e7a9011ba66a9ddbca336c2a3d
Jackie <jackierhammer@gmail.com>
2022-11-14 20:04:11 -0700

    added post model

--- models/Post.js
+++ models/Post.js
@@ -1,1 +1,29 @@
 // this is where the post model will go
+
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection.js');
+
+// creates Post model
+class Post extends Model {}
+
+// posts only need a title and body
+// the connection to the user will be a foreign key relationship
+Post.init(
+  {
+    // both title and body are required elements
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    body: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    }
+  },
+  {
+    sequelize
+  }
+);
+
+// exports Post model
+module.exports = Post;
