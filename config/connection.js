627bf80c660f6e6341130f447e4cd15e2f8743eb
Jackie <jackierhammer@gmail.com>
2022-11-14 19:09:50 -0700

    set up connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,1 +1,25 @@
 // this is where the server connection is created
+
+// imports sequelize so we can use to create server
+const Sequelize = require('sequelize');
+// imports dotenv so we can use environmental variables to protect passwords
+require('dotenv').config();
+
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+    sequelize = new Sequelize(
+        process.env.DB_NAME,
+        process.env.DB_USER,
+        process.env.DB_PASSWORD,
+        {
+            host: 'localhost',
+            dialect: 'mysql',
+            port: 3306
+        }
+    );
+}
+
+module.exports = sequelize;
