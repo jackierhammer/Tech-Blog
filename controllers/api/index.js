ef4096c10cae65a916b5dca6d5bebeea44ef8529
Jackie <jackierhammer@gmail.com>
2022-11-14 20:38:12 -0700

    finished controllers/api/index.js router setup

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,1 +1,17 @@
 // this is where user, post, and comment routes will be added to the router
+
+// the router
+const router = require('express').Router();
+
+// model routes
+const userRoutes = require('./user-routes.js');
+const postRoutes = require('./post-routes');
+const commentRoutes = require('./comment-routes');
+
+// connects each set of routes to its api path
+router.use('/user', userRoutes);
+router.use('/post', postRoutes);
+router.use('/comment', commentRoutes);
+
+// exports updated router
+module.exports = router;
