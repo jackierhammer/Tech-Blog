c3d1a9fdf37e284987dc5ac6233b20b3a03e4a37
Jackie <jackierhammer@gmail.com>
2022-11-14 22:01:04 -0700

    updated router in controllers/index.js

--- controllers/index.js
+++ controllers/index.js
@@ -1,1 +1,17 @@
 // this is where dashboard routes, home routes, and api routes will be added to the router
+
+// imports router
+const router = require('express').Router();
+
+// import statements for routes 
+const apiRoutes = require("./api")
+const dashboardRoutes = require('./dashboard-routes.js');
+const homeRoutes = require('./home-routes');
+
+// connects routes to their paths
+router.use('/api', apiRoutes);
+router.use('/dashboard', dashboardRoutes);
+router.use('/', homeRoutes);
+
+// exports updated router
+module.exports = router;
