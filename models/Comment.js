4d00128595be84e310044744715caa328d7a2a9d
Jackie <jackierhammer@gmail.com>
2022-11-14 19:43:07 -0700

    created user model

--- models/Comment.js
+++ models/Comment.js
@@ -1,1 +1,2 @@
 // this is where the comment model will go
+
--- models/User.js
+++ models/User.js
@@ -1,1 +1,63 @@
 // this is where the user model will go
+
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+
+// creates User model
+class User extends Model {
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+    }
+}
+
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            // makes sure another user doesn't exist with this email
+            unique: true,
+            // checks that the input is in valid email format
+            validate: {
+                isEmail: true,
+            },
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+    },
+    {
+        hooks: {
+            // hashes new passwords for security
+            beforeCreate: async (newUserData) => {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+            },
+            // hashes updated passwords for security
+            beforeUpdate: async (updatedUserData) => {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData;
+            },
+        },
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user',
+    }
+);
+
+// exports User model
+module.exports = User;
