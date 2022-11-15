b16f287225d7f8954069241057b3aa3dc1e91161
Jackie <jackierhammer@gmail.com>
2022-11-14 22:37:19 -0700

    finished with routes until handlebars pages are done

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,1 +1,65 @@
 // this is where dashboard routes will go
+
+// imports router
+const router = require("express").Router();
+// imports post model
+// we don't use the other models, so no need to import
+const { Post } = require("../models");
+// imports withAuth helper function
+const withAuth = require("../utils/auth");
+
+// route for a user seeing all of their own posts (admin)
+router.get("/", withAuth, (req, res) => {
+    Post.findAll({
+        // this is how we find only the user's posts
+        where: {
+            userId: req.session.userId
+        }
+    })
+        .then(dbPostData => {
+            const posts = dbPostData.map((post) => post.get({ plain: true }));
+            // commented out because no handlebars yet
+            // res.render("all-posts-admin", {
+            //     layout: "dashboard",
+            //     posts
+            // });
+        })
+        .catch((err) => {
+            console.log(err);
+            // redirects the user to login page 
+            // handlebars blah blah blah
+            // res.redirect("login");
+        });
+});
+
+// route for new post form
+router.get("/new", withAuth, (req, res) => {
+    // renders the new post template
+    // res.render("new-post", {
+    //     layout: "dashboard"
+    // });
+});
+
+// route for edit post form
+router.get("/edit/:id", withAuth, (req, res) => {
+    Post.findByPk(req.params.id)
+        .then(dbPostData => {
+            if (dbPostData) {
+                const post = dbPostData.get({ plain: true });
+                // something something handlebars
+                // res.render("edit-post", {
+                //     layout: "dashboard",
+                //     post
+                // });
+            } else {
+                // post not found error
+                res.status(404).end();
+            }
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        });
+});
+
+// exports updated router
+module.exports = router;
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,1 +1,73 @@
 // this is where home routes will go
+
+// imports router
+const router = require("express").Router();
+
+// imports models
+const { Post, Comment, User } = require("../models");
+
+// route for finding all posts for homepage
+router.get("/", (req, res) => {
+// finds all posts and their associated users
+    Post.findAll({
+        include: [User],
+    })
+        .then((dbPostData) => {
+            const posts = dbPostData.map((post) => post.get({ plain: true }));
+            // will uncomment when handlebars is created for this
+            // res.render("all-posts", { posts });
+        })
+        .catch((err) => {
+            res.status(500).json(err);
+        });
+});
+
+// route for finding a single post
+router.get("/post/:id", (req, res) => {
+    Post.findByPk(req.params.id, {
+        include: [
+            // includes user who posted and comments and users who posted those
+            User,
+            {
+                model: Comment,
+                include: [User],
+            },
+        ],
+    })
+        .then((dbPostData) => {
+            if (dbPostData) {
+                const post = dbPostData.get({ plain: true });
+                // will uncomment when handlebars is created for this
+                // res.render("single-post", { post });
+            } else {
+                // not found error
+                res.status(404).end();
+            }
+        })
+        .catch((err) => {
+            res.status(500).json(err);
+        });
+});
+
+// login route
+router.get("/login", (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect("/");
+        return;
+    }
+    // will uncomment...handlebars...
+    // res.render("login");
+});
+
+// signup route
+router.get("/signup", (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect("/");
+        return;
+    }
+    // will uncomment...handlebars...
+    // res.render("signup");
+});
+
+// exports updated router
+module.exports = router;
