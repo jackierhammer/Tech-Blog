7126d07ec6a11071e418d57a9f580344b113ddb2
Jackie <jackierhammer@gmail.com>
2022-11-15 19:16:10 -0700

    updated front end js files relating to post models

--- public/js/edit.js
+++ public/js/edit.js
@@ -1,1 +1,29 @@
 // this is where comment editing functionality will go
+
+// function handles editing existing posts
+const editFormHandler = async function (event) {
+    event.preventDefault();
+    // stores the post information in variables to use in request
+    const titleEl = document.getElementById('post-title');
+    const contentEl = document.getElementById('post-content');
+    const postId = document.getElementById('post-id')
+    // put request
+    fetch("/api/post/" + postId.value, {
+        method: "put",
+        body: JSON.stringify({
+            title: titleEl.value,
+            content: contentEl.value
+        }),
+        headers: { "Content-Type": "application/json" }
+    })
+        .then(function () {
+            // redirects user to the dashboard
+            document.location.replace("/dashboard");
+        })
+        .catch(err => console.log(err))
+}
+
+// waits for user to submit to update the information
+document
+    .querySelector("#edit-post-form")
+    .addEventListener("submit", editFormHandler);
--- public/js/new.js
+++ public/js/new.js
@@ -1,1 +1,27 @@
 // this is where new post making functionality will go
+
+// function handles new post post request
+async function newFormHandler(event) {
+    event.preventDefault();
+    //   stores the post's title and content in variables
+    const title = document.querySelector('input[name="post-title"]').value;
+    const content = document.querySelector('input[name="content"]').value;
+
+    const response = await fetch(`/api/posts`, {
+        method: 'POST',
+        body: JSON.stringify({
+            title,
+            content
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+    //   redirects user to dashboard after posting
+    document.location.replace('/dashboard');
+};
+
+// waits for user to submit before processing the request
+document
+    .querySelector('#new-post-form')
+    .addEventListener('submit', newFormHandler);
