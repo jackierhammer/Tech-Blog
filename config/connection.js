1622e4b86697e53b0d14355ff09d857e78446720
Jackie <jackierhammer@gmail.com>
2022-11-15 23:16:10 -0700

    jawsdb edit

--- config/connection.js
+++ config/connection.js
@@ -7,8 +7,8 @@ require('dotenv').config();
 
 let sequelize;
 
-if (process.env.JAWSDB_URL) {
-    sequelize = new Sequelize(process.env.JAWSDB_URL);
+if (process.env.JAWS_DB_URL {
+    sequelize = new Sequelize(process.env.JAWS_DB_URL);
 } else {
     sequelize = new Sequelize(
         process.env.DB_NAME,
