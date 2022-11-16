e3c5bc65a6ee9e065254b8a454961bc5c94de207
Jackie <jackierhammer@gmail.com>
2022-11-15 18:57:03 -0700

    worked on login and logout front end functionality

--- public/js/login.js
+++ public/js/login.js
@@ -1,1 +1,28 @@
 // this is where login functionality will go
+
+// function handles post request 
+const loginFormHandler = async function (event) {
+    event.preventDefault();
+    // stores user input for the request
+    const usernameEl = document.querySelector("#username-input-login");
+    const passwordEl = document.querySelector("#password-input-login");
+    // api post request
+    fetch("/api/user/login", {
+        method: "post",
+        body: JSON.stringify({
+            username: usernameEl.value,
+            password: passwordEl.value
+        }),
+        headers: { "Content-Type": "application/json" }
+    })
+        .then(() => {
+            // navigates to the site dashboard
+            document.location.replace("/dashboard");
+        })
+        .catch(err => console.log(err));
+};
+
+// waits for user to submit to process request
+document
+    .querySelector("#login-form")
+    .addEventListener("submit", loginFormHandler);
--- public/js/logout.js
+++ public/js/logout.js
@@ -1,1 +1,20 @@
 // this is where logout functionality will go
+
+// this function handles logging user out request
+function logout() {
+    // post request
+    fetch("/api/user/logout", {
+        method: "post",
+        headers: { "Content-Type": "application/json" }
+    })
+        .then(() => {
+            // redirects to homepage
+            document.location.replace("/");
+        })
+        .catch(err => console.log(err));
+}
+
+// waits for user to click on logout before logging out
+document
+    .querySelector("#logout-link")
+    .addEventListener("click", logout);
